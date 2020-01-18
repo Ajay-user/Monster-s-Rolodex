e916731a1a0f9d34d2c497ec3cf38375c21ef065
Ajay <ajay@gmail>
2020-01-18 17:46:12 +0530

    Updates

---
+++ precache-manifest.599540eea72ba2d0a6d2626080751484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d50756789f09c6ad5c6da32ca9b8b9",
+    "url": "/Monster-s-Rolodex/index.html"
+  },
+  {
+    "revision": "e141403e22688a5837e7",
+    "url": "/Monster-s-Rolodex/static/css/main.05c16489.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/Monster-s-Rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monster-s-Rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e141403e22688a5837e7",
+    "url": "/Monster-s-Rolodex/static/js/main.00c62af1.chunk.js"
+  },
+  {
+    "revision": "7ac7324607aeed631dd9",
+    "url": "/Monster-s-Rolodex/static/js/runtime-main.5d23fcba.js"
+  }
+]);
